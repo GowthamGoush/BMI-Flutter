cec116d2d5aeed2e591ab1c28cdc1c6c61328826
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-09 13:44:50 +0530

    Update main dart class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:bmi_flutter_app/inputPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(),
+    theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF2E3551),
+        scaffoldBackgroundColor: Color(0xFF21273E)),
+    home: InputPage(),
   ));
 }
