207434ec875193cbadb2a094b8b186b57597c38e
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-10 12:54:47 +0530

    Add constants class

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+const int kPrimaryColor = 0xFF2E3551;
+const int kScaffoldBackgroundColor = 0xFF21273E;
+const int kAppBarColor = 0xFF414772;
+const int kBaseCardColor = 0xB3272A4D;
+const int kActiveCardColor = 0x80414772;
+const int kFontColor = 0xFF8D8E98;
