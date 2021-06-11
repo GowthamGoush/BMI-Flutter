943b30df986ec4901df2371156c3937e96bf8d0f
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-11 11:20:30 +0530

    Add custom text widget class

---
+++ lib/customTextStyle.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'constants.dart';
+
+class CustomTextStyle extends StatelessWidget {
+  CustomTextStyle(
+      {required this.textLabel,
+      required this.fontSize,
+      this.fontColor = kFontColor,
+      this.fontWeight = FontWeight.normal,
+      this.letterSpacing = 3.5});
+
+  final String textLabel;
+  final double fontSize;
+  final double letterSpacing;
+  final int fontColor;
+  final FontWeight fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      textLabel,
+      style: GoogleFonts.balooTammudu(
+        color: Color(fontColor),
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        letterSpacing: letterSpacing,
+      ),
+    );
+  }
+}
