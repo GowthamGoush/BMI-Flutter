9cdf5cc861fbeb514d9cf064aba56aac66a668c7
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-13 10:01:20 +0530

    Update constants resource

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 const int kPrimaryColor = 0xFF2E3551;
 const int kScaffoldBackgroundColor = 0xFF21273E;
 const int kAppBarColor = 0xFF414772;
@@ -7,3 +9,7 @@ const int kFontColor = 0xFF8D8E98;
 const int kDefaultFontColor = 0xFFFFFFFF;
 const int kBottomContainerColor = 0xFFEB1555;
 const double kBottomContainerHeight = 80.0;
+const kResultTextStyle = TextStyle(
+    fontSize: 18.0, color: Color(0xFF24D876), fontWeight: FontWeight.bold);
+const kBMITextStyle = TextStyle(fontSize: 96.0, fontWeight: FontWeight.bold);
+const kBodyTextStyle = TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
